78db49d5bfec9ddd212fdde367181a43bf35b879
Viki08viki <vignesh0808viki@gmail.com>
2026-07-13 07:34:14 +0000

    feat: connect Flask API to MariaDB

--- app/backend/app.py
+++ app/backend/app.py
@@ -1,6 +1,15 @@
 from flask import Flask, jsonify
+import mysql.connector
 
 app = Flask(__name__)
+
+def get_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        user="cloudcart",
+        password="CloudCart@123",
+        database="cloudcart"
+    )
 
 @app.route("/")
 def home():
@@ -14,5 +23,18 @@ def health():
         "status": "UP"
     })
 
+@app.route("/products")
+def products():
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM products")
+    result = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return jsonify(result)
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)  
